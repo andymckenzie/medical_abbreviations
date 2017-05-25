30bcc03877f9c4fb32322ee446f1d475bdb293a3
Andy McKenzie <amckenz@gmail.com>
2017-05-25 08:43:18 -0400

    First commit

---
+++ reddit_bot.py
@@ -0,0 +1,10 @@
+
+#http://progur.com/2016/09/how-to-create-reddit-bot-using-praw4.html
+
+#loads table of acronyms, actual phrases, +/- defintions
+
+#streams reddit comments from medical subreddits
+
+#posts a comment saying what the abbreviation actually means
+
+#TODO: asks for feedback
